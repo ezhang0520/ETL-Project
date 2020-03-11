c18ea9f684713b76c272cdfd8ae3e08c98b1d712
flowerstarad@aol.com <flowerstarad@aol.com>
2020-03-10 21:05:36 -0400

    Add files

--- ETL-Project.sql
+++ ETL-Project.sql
@@ -3,3 +3,15 @@ FROM consumption
 JOIN excessive_drinking
 ON consumption.year = excessive_drinking.year;
 
+create table consumption (
+	state varchar PRIMARY KEY,
+	year INT,
+	consumption_amount varchar
+)
+
+create table excessive_drinking (
+	year int PRIMARY KEY,
+	consumption varchar,
+	state varchar,
+	rank INT
+)
---
+++ binge_drinking_create_table.sql
@@ -0,0 +1,11 @@
+create table binge_drinking(
+year int,
+location_abbr text,
+location varchar,
+topic varchar,
+response text,
+value_unit varchar,
+value_type varchar,
+data_value float,
+sample_size int
+);
